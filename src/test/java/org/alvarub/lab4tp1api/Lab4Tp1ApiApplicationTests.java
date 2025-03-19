ded0b93ab88293db23a09247874ae4b658b78031
Alvaro Rubina <alvarorubina132@gmail.com>
2025-03-19 18:36:27 -0300

    primer commit

---
+++ src/test/java/org/alvarub/lab4tp1api/Lab4Tp1ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alvarub.lab4tp1api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4Tp1ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
